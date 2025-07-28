f2d406c8c85d99b37bdd8cf97af741a5eb109c9f
Shubham Jalwaniya <jalwaniyash702@gmail.com>
2025-07-28 17:43:40 +0530

    cros is added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import { connectDB } from './lib/db.js'
 import mongoose from 'mongoose'
 import cookieParser from "cookie-parser" 
+import cors from 'cors'
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
@@ -16,6 +17,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/XChat')
 .then(()=> console.log('MonogDB connected'))
 .catch(error =>console.log(error));
 
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials: true // allow frontend to send cookies
+}))
 app.use(express.json());
 app.use(cookieParser());
 
