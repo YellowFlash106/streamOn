3952cb38a7596212a1732c464dbf96f53346c923
Shubham Jalwaniya <jalwaniyash702@gmail.com>
2025-08-02 10:23:39 +0530

    git  initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './lib/db.js'
 import mongoose from 'mongoose'
 import cookieParser from "cookie-parser" 
 import cors from 'cors'
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
@@ -11,11 +12,13 @@ import chatRoutes from "./routes/chat.route.js"
 
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5002
 
-mongoose.connect('mongodb://127.0.0.1:27017/XChat')
-.then(()=> console.log('MonogDB connected'))
-.catch(error =>console.log(error));
+const __dirname = path.resolve();
+
+// mongoose.connect('mongodb://127.0.0.1:27017/XChat')
+// .then(()=> console.log('MonogDB connected'))
+// .catch(error =>console.log(error));
 
 app.use(cors({
     origin : "http://localhost:5173",
@@ -28,7 +31,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
 app.listen(PORT, ()=>{
     console.log(`Server is runing on this port ${PORT}`);
-    // connectDB();
+    connectDB();
 })
